f07b7496380310af7a15143b595e7207d7da93f6
erena <eretan1233@docomo.ne.jp>
2024-09-10 05:29:46 +0000

    課題

--- php_practice/kadai.php
+++ php_practice/kadai.php
@@ -9,6 +9,29 @@ echo $array_month[7];
 
 $hello ="Hello,";
 $name ="Erena";
-$world ="s World";
+$world ="'s World";
 $hello.=$name.=$world;
 echo $hello;
+
+$tech_boost ="tech";
+$tech_boost.="boost";
+echo $tech_boost;
+
+
+$a2018_calendar = [
+    "January" => "1月",
+    "February" => "2月",
+    "March" => "3月",
+    "April" => "4月",
+    "May" => "5月",
+    "June" => "6月",
+    "July" => "7月",
+    "August" => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "November" => "11月",
+    "December" => "12月"
+];
+echo $a2018_calendar["December"];
+
+  
