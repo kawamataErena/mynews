69e9c7d47ed62ff8967da330b041aea98ec9e344
erena <eretan1233@docomo.ne.jp>
2024-09-10 02:30:25 +0000

    課題

---
+++ php_practice/kadai.php
@@ -0,0 +1,14 @@
+<?php
+$a =3;
+$b =7;
+
+echo $a+$b;
+
+$array_month =["１月","２月","３月","４月","５月","６月","７月","８月","９月","１０月","１１月","１２月"];
+echo $array_month[7];
+
+$hello ="Hello,";
+$name ="Erena";
+$world ="s World";
+$hello.=$name.=$world;
+echo $hello;
