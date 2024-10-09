2a3354ea4523fcf4991be12d77bd76a5822c9488
erena <eretan1233@docomo.ne.jp>
2024-10-09 16:32:50 +0000

    課題14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace app\Http\Controllers\admin;
 use app\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\profile;
+
 class ProfileController extends Controller
 {
     //以下を追記
@@ -13,8 +15,17 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        
+    $this->validate($request, profile::$rules);
+
+    $profile = new profile;
+    $form = $request->all();
+
+    $profile ->fill($form);
+    $profile ->save();
+
         return redirect('admin/profile/create');
     }
 
@@ -27,4 +38,7 @@ class ProfileController extends Controller
     {
         return redirect('admin/profile/edit');
     }
+
+   
+
 }
